073b7db0275d40132b7dca7eb141a8bfd2635b93
NassarX <mahmoud@nassarx.com>
2023-05-26 22:11:51 +0100

    Remove unused code from the main

--- app/main.py
+++ app/main.py
@@ -1,8 +1,4 @@
-from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
-import time
-from app.nqueens.backtracking import NQueensBacktracking
-from app.nqueens.utils import state_to_string
+from fastapi import FastAPI
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -11,29 +7,3 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/")
 def check():
     return "Welcome to NQueens Puzzle Optimizations!"
-
-
-@app.get("/backtracking/{n}")
-def backtracking(request: Request, n: int):
-    solution = NQueensBacktracking()
-    start_time = time.time()
-    solutions = solution.solveNQueens(n)
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-
-    board = []
-    for solution in solutions:
-        board.append(state_to_string(solution, n))
-
-    return templates.TemplateResponse("index.html", {
-        "request": request,
-        "solutions": board,
-        "elapsed_time": elapsed_time,
-        "n": n,
-        "number_of_solutions": len(solutions)
-    })
-
-
-@app.get("/hill_climbing/{n}")
-def backtracking(request: Request, n: int):
-    return 'Not yet'
